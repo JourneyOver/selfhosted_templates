bdf182145fafcf4fa07b422222022dc3d6d1d628
Journey <timtag1190@gmail.com>
2022-07-21 16:07:46 -0500

    add a joinapps script to make making the template a lot easier..
    Pulled from https://github.com/novaspirit/pi-hosted/blob/master/build/joinApps.sh and then edited to work with only 1 template.

---
+++ build/joinApps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Blank portainer template
+json='{"version":"2","templates":[]}'
+
+# Main Template
+template='template/portainer-v2.json'
+
+# Run script from base directory
+scriptDir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$scriptDir/.." || exit
+
+# Parsing all json files in apps folder
+for app in template/apps/*.json; do
+	# Output app name to easy debug
+	echo "adding $app to template..."
+
+	appjson=$( cat "$app" )
+
+	if [[ -n "$appjson" ]]; then
+		json=$( echo "$json" | jq --argjson newApp "$appjson" '.templates += [$newApp]' )
+	fi
+
+	# clean variables before next loop
+	unset appjson
+done
+
+# Create Templates
+echo "$json" | jq --indent 2 '.templates |= sort_by(.title | ascii_upcase)' > "$template"
