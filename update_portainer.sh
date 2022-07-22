11d326d998fae558515893fad7bc0eeb4f24092c
Journey <timtag1190@gmail.com>
2022-07-21 22:46:44 -0500

    seems like an install script is needed to be used before joal container is created
    otherwise it just throws errors about "folders" and "files" not existing.

--- update_portainer.sh
+++ update_portainer.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-function error {
-  echo -e "\\e[91m$1\\e[39m"
-  exit 1
-}
-
-function check_internet() {
-  printf "Checking if you are online..."
-  wget -q --spider http://github.com
-  if [ $? -eq 0 ]; then
-    echo "Online. Continuing."
-  else
-    error "Offline. Go connect to the internet then run the script again."
-  fi
-}
-
-check_internet
-
 portainer_pid=`docker ps | grep portainer-ce | awk '{print $1}'`
 portainer_name=`docker ps | grep portainer-ce | awk '{print $2}'`
 
