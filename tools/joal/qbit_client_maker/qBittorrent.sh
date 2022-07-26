242179b6cbf89bd5c7a9cb3648657369a5ffdb27
Journey <timtag1190@gmail.com>
2022-07-25 21:27:05 -0500

    Add Prowlarr
    remove network from nginx as it doesn't seem to change automatically to "host".. so just have to remember to do it manually if I ever redeploy this on another computer.

--- tools/joal/qbit_client_maker/qBittorrent.sh
+++ tools/joal/qbit_client_maker/qBittorrent.sh
@@ -64,9 +64,9 @@ echo "key : qBittorent is using libtorrent => $(libtorrent_get_key_format)"
 
 # Make Client File
 BlankFile="./blankclients/qbittorrent-0.0.0.client"
-NewClient="/portainer/Files/AppData/Config/Joal/config/clients/qbittorrent-$QBT_VERSION_2.client"
+NewClient="/portainer/Files/AppData/Config/Joal/clients/qbittorrent-$QBT_VERSION_2.client"
 cp $BlankFile $NewClient && sed -i "s/-qB0000-/$peer_id_prefix/;s/0.0.0/$QBT_VERSION_2/" $NewClient
-sed -i "s/utorrent-3.5.0_43916/qbittorrent-$QBT_VERSION_2/" /portainer/Files/AppData/Config/Joal/config/config.json
+sed -i "s/utorrent-3.5.0_43916/qbittorrent-$QBT_VERSION_2/" /portainer/Files/AppData/Config/Joal/config.json
 
 # clean tempSource folder
 rm -rf $qBittorrentTempFolder
