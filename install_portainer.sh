4adf82a5d1e13ca2931275da9254f757a74d1091
Journey <timtag1190@gmail.com>
2022-07-26 05:38:28 -0500

    Create a network before installing portainer
    network will end up being used by all services.

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo docker network create bunni_network
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d -p 9001:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+sudo docker run -d --network=bunni_network -p 9001:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
