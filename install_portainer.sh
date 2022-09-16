03323d012f165fec0b08a33e0eb09077514cd680
Journey <timtag1190@gmail.com>
2022-09-16 03:17:13 -0500

    Portainer Business Edition

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 sudo docker network create bunni_network
-sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+# Portainer Business Edition
+sudo docker pull portainer/portainer-ee:latest || error "Failed to pull latest Portainer docker image!"
+sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest || error "Failed to run Portainer docker image!"
+
+# Portainer Community Edition
+#sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
+#sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
