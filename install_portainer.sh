12bea5323af0613bdea136b86c2b880cdc94d830
Journey <timtag1190@gmail.com>
2022-09-16 01:53:00 -0500

    Update portainer ports

--- install_portainer.sh
+++ install_portainer.sh
@@ -2,4 +2,4 @@
 
 sudo docker network create bunni_network
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d --network=bunni_network -p 9001:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
