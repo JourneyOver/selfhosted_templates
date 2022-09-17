0f6cd154329528e48ef299f09c0cd3d695d4820f
Journey <timtag1190@gmail.com>
2022-09-17 00:56:15 -0500

    More stacks

--- install_portainer.sh
+++ install_portainer.sh
@@ -3,8 +3,8 @@
 sudo docker network create bunni_network
 # Portainer Business Edition
 sudo docker pull portainer/portainer-ee:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest || error "Failed to run Portainer docker image!"
+sudo docker run -d --network=bunni_network -e TZ=America/Chicago -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest || error "Failed to run Portainer docker image!"
 
 # Portainer Community Edition
 #sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-#sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+#sudo docker run -d --network=bunni_network -e TZ=America/Chicago -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
