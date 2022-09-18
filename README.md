ce7c408cbba30ff223ffe936959f6b93230702d7
Journey <timtag1190@gmail.com>
2022-09-18 00:38:37 -0500

    First round of testing a couple things.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Personal Portainer Templates for Selfhosted Projects
 
-This is mainly for myself, so it'll only have apps that I am hosting. This template will be useless for pretty much anyone else.
+This template will only be useful for myself, feel free to take ideas from this repository if you come across it and want one of the apps in it, but do not make request or make issues as they will be ignored.
+
+Most ideas came from: [pi-hosted](https://github.com/pi-hosted/pi-hosted)
--- update_portainer.sh
+++ update_portainer.sh
@@ -6,8 +6,4 @@ portainer_name=`docker ps -a | grep portainer | awk '{print $2}'`
 sudo docker stop $portainer_pid || error "Failed to stop portainer!"
 sudo docker rm $portainer_pid || error "Failed to remove portainer container!"
 sudo docker rmi $portainer_name || error "Failed to remove/untag images from the container!"
-# Portainer Business Edition
-sudo docker run -d --network=bunni_network -e TZ=America/Chicago -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest || error "Failed to execute newer version of Portainer!"
-
-# Portainer Community Edition
-#sudo docker run -d --network=bunni_network -e TZ=America/Chicago -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to execute newer version of Portainer!"
+sudo docker run -d --network=bunni_network -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to execute newer version of Portainer!"
